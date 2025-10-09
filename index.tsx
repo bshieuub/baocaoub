6cc5fedd18a9ac48b436d9ba638612df396e2e08
bshieuub <51667204+bshieuub@users.noreply.github.com>
2025-10-09 00:13:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ index.tsx
@@ -0,0 +1,3 @@
+// QUAN TRỌNG: Tệp này phải được xóa.
+// Sự tồn tại của nó cùng với 'index.js' gây nhầm lẫn cho máy chủ triển khai và là nguyên nhân gốc rễ của lỗi "Uncaught SyntaxError: Unexpected token '<'".
+// Vui lòng XÓA tệp này khỏi dự án của bạn để khắc phục sự cố.
---
+++ types.js
@@ -0,0 +1,3 @@
+// To resolve a deployment error, all JavaScript code has been consolidated
+// into the <script> tag within `index.html`.
+// This file is no longer used.
